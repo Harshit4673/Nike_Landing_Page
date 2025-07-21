2d5ae2a511ea238fdbb6df1034767c3748ca7782
Harshit Chaurasia <harshitchaurasia4673@gmail.com>
2025-07-21 22:40:18 +0530

    fixes

--- landingpage1/src/App.jsx
+++ landingpage1/src/App.jsx
@@ -3,7 +3,7 @@ import Featured from "./components/featured/Featured";
 import Football from "./components/football/Football";
 import Footer from "./components/footer/Footer";
 import Navigation from "./components/navigation/Navigation";
-import Poster from "./components/poster/poster";
+import Poster from "./components/poster/Poster";
 import {slides} from './data/carouselData.json'
 
 const App = () => {
